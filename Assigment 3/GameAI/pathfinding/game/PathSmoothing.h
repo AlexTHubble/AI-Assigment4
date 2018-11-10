dceb9c1e83b3c87425164e10b9eb5e770e2b0f92
Josh Grazda <joshuagrazda@yahoo.com>
2018-11-10 15:31:06 -0500

    PathSmoothing files added Added: - PathSmoothin files

---
+++ Assigment 3/GameAI/pathfinding/game/PathSmoothing.h
@@ -0,0 +1,16 @@
+#ifndef PATH_SMOOTHING_H
+#define PATH_SMOOTHING_H
+
+#include <map>
+#include <Trackable.h>
+
+class PathSmoothing : public Trackable
+{
+public:
+
+private:
+
+};
+
+#endif // !PATH_SMOOTHING_H
+
