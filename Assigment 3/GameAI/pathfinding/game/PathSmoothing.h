17fdac293f14de10402f6b7d7df8131021fd35ca
Josh Grazda <joshuagrazda@yahoo.com>
2018-11-11 14:57:52 -0500

    PathSmoothing code finished. Untested Changed: - Rest of RayCast code completed from https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/

--- Assigment 3/GameAI/pathfinding/game/PathSmoothing.h
+++ Assigment 3/GameAI/pathfinding/game/PathSmoothing.h
@@ -2,6 +2,7 @@
 #define PATH_SMOOTHING_H
 
 #include "GridPathfinder.h"
+#include <Vector2D.h>
 #include <vector>
 #include <map>
 #include <Trackable.h>
@@ -20,6 +21,11 @@ public:
 	//
 	Path* SmoothPath(Graph* pGraph, Path* pPath);
 	bool RaycastToTarget(Node* current, Node* target);
+	//
+	bool LineSquareIntersection(Node* current, Node* target, Node* wallNode);
+	bool LineLineIntersection(Vector2D p1, Vector2D q1, Vector2D p2, Vector2D q2);
+	bool OnSegment(Vector2D p, Vector2D q, Vector2D r);
+	int Orientation(Vector2D p, Vector2D q, Vector2D r);
 
 private:
 	std::vector<Node*> wallBlocks;
